05e420473db108f86ef8aa49fc0efb988500f6ac
kitajk <129586105+kitajk@users.noreply.github.com>
2023-12-13 11:43:06 +0000

    Added changing the player name triggered by 'Enter' key

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,36 +1,49 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Player({ name, symbol }) {
-	// useState for editing button and changing span into input
-	const [isEditing, setIsEditing] = useState(false)
-	// useState for getting player name as value
-	const [inputValue, setInputValue] = useState(name)
+  // useState for editing button and changing span into input
+  const [isEditing, setIsEditing] = useState(false);
+  // useState for getting player name as value
+  const [inputValue, setInputValue] = useState(name);
 
-	function handleEdit() {
-		// This fragment of code is responsible for preventing user from entering empty player name
-		if (isEditing) {
-			if (inputValue.trim() !== '') {
-				setInputValue(inputValue)
-				setIsEditing(false)
-			}
-		} else {
-			setIsEditing(true)
-		}
-	}
+  function handleEdit() {
+    // This fragment of code is responsible for preventing user from entering empty player name
+    if (isEditing) {
+      if (inputValue.trim() !== "") {
+        setInputValue(inputValue);
+        setIsEditing(false);
+      }
+    } else {
+      setIsEditing(true);
+    }
+  }
 
-	return (
-		<li>
-			<span className='player'>
-				{/* Setting span or input depending on isEditing value */}
-				{isEditing ? (
-					<input id='name' type='text' required value={inputValue} onChange={e => setInputValue(e.target.value)} />
-				) : (
-					<span className='player-name'>{inputValue}</span>
-				)}
-				<span className='player-symbol'>{symbol}</span>
-			</span>
-			{/* Setting appropriate button text */}
-			<button onClick={handleEdit}>{isEditing ? 'OK' : 'Edit'}</button>
-		</li>
-	)
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      handleEdit();
+    }
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {/* Setting span or input depending on isEditing value */}
+        {isEditing ? (
+          <input
+            id="name"
+            type="text"
+            required
+            value={inputValue}
+            onKeyUp={handleEnter}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        ) : (
+          <span className="player-name">{inputValue}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      {/* Setting appropriate button text */}
+      <button onClick={handleEdit}>{isEditing ? "OK" : "Edit"}</button>
+    </li>
+  );
 }
